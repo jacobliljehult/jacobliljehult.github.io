fdb07d5607909832e2d528ca5a050328654075c3
jacobliljehult <jacob.mesot.liljehult@regionh.dk>
2022-01-25 17:07:30 +0100

    test

---
+++ test.py
@@ -0,0 +1,8 @@
+a = input("Indtast et tal: ")
+b = input("Indtast endnu et tal: ")
+if a==b:
+    print("They are equal")
+elif a > b:
+    print("A is greater than B")
+else:
+    print("B is greater than A") 
