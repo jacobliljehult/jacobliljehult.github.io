84094bd1af94f4f5f72d1aa26f63f5244c259db2
jacobliljehult <jacob.mesot.liljehult@regionh.dk>
2022-09-04 12:03:23 +0200

    var

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-a = input("Indtast et tal: ")
-b = input("Indtast endnu et tal: ")
-if a==b:
-    print("They are equal")
-elif a > b:
-    print("A is greater than B")
-else:
-    print("B is greater than A") 
+def new_func():
+    msg = "WTF"
+    print(msg)
+
+new_func()
